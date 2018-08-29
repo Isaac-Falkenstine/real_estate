1e49c9eaae311d1045aa9de75e02c03e3d587554
Isaac Falkenstine <isaac@falkenstine.com>
2018-08-29 13:53:15 -0600

    Added more functionality to the House class

--- lib/house.rb
+++ lib/house.rb
@@ -21,4 +21,22 @@ class House
   def add_room(room)
     @house_rooms << room
   end
+
+  def rooms_from_category(category)
+    category_array = @house_rooms.map do |room|
+      if room.category == category
+        room
+      end
+    end
+    category_array.compact
+  end
+
+  def area
+    area_total = 0
+    @house_rooms.each do |room|
+      room_area = room.height * room.base
+      area_total += room_area
+    end
+    area_total
+  end
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -32,4 +32,32 @@ class HouseTest < MiniTest::Test
     assert_instance_of Room, house.rooms[0]
     assert_instance_of Room, house.rooms[1]
   end
+
+  def test_can_sort_rooms_by_category
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    assert_equal :bedroom, house.rooms_from_category(:bedroom)[0].category
+    assert_equal :bedroom, house.rooms_from_category(:bedroom)[1].category
+    assert_equal :basement, house.rooms_from_category(:basement)[0].category
+  end
+
+  def test_area_works_with_multiple_rooms
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    assert_equal 1900, house.area
+  end
 end
